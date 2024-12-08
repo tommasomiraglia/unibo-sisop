961e62872fafddae9e7586f35d68edf3f07b5f7b
Tommaso Miraglia <tommaso.miraglia@studio.unibo.it>
2024-12-08 16:26:00 +0100

    sto studiando

---
+++ seconda.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#~ Scrivere un file contenente alcune (almeno 5) righe di testo, ciascuna con almeno 4
+#~ parole. Scrivere uno script seconda.sh che prende come unico argomento il nome
+#~ di quel file. Dentro lo script utilizzare ripetutamente il comando read per leggere la
+#~ seconda parola di ogni riga del file. Tutte le seconde parole devono essere
+#~ concatenata in una variabile di nome OUT. Alla fine dello script, la variabile OUT
+#~ deve essere visualizzata sullo standard output
+
+OUT=""
+while read A B C;do
+	OUT=${OUT}${B} #concatenare le stringhe
+done < $1
+echo $OUT
